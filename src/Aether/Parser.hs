983c8805c795ea54b1e425e2e170c45bcfffc488
Allen Guo <guoguo12@gmail.com>
2013-11-04 19:16:41 -0500

    Add documentation

--- src/Aether/Parser.hs
+++ src/Aether/Parser.hs
@@ -1,4 +1,18 @@
-﻿module Aether.Parser ( extractBetween
+﻿-----------------------------------------------------------------------------
+-- |
+-- Module    : Aether.Parser
+-- Copyright : (c) Allen Guo 2013
+-- License   : MIT
+-- 
+-- Maintainer : Allen Guo <guoguo12@gmail.com>
+-- Stability  : alpha
+--
+-- The 'Parser' module contains several simple XML-parsing
+-- functions that use regular expressions, as well as other
+-- text utility functions.
+--
+-----------------------------------------------------------------------------
+module Aether.Parser ( extractBetween
                      , extractAllAttrValues
                      , trim
                      ) where
@@ -6,21 +20,31 @@
 import Data.Text (pack, strip, unpack)
 import Text.Regex (matchRegex, mkRegex, mkRegexWithOpts, splitRegex)
 
+-- | Given a list of strings, returns those strings that are 
+-- non-null as a list.
 nonNull :: [String] -> [String]
 nonNull = filter (/= [])
 
+-- | Returns the given string with whitespace trimmed.
 trim :: String -> String
 trim = unpack . strip . pack
 
+-- | A utility regex function that returns the first match found with
+-- the given text and regex string. Returns an empty string if no matches
+-- are found.
 stdRegex :: String -> String -> String
 stdRegex text regex =
   case matchRegex (mkRegexWithOpts regex False False) text of
     Nothing      -> ""
     Just matches -> head matches
 
+-- | @extractBetween text start end@ will return the portion of @text@ between
+-- @start@ and @end@ as a string.
 extractBetween :: String -> String -> String -> String
 extractBetween text start end = stdRegex text $ start ++ "(.*)" ++ end
 
+-- | @extractAllAttrValues text attr@, where @text@ is a XML string, returns
+-- the values of all instsances of the XML attribute @attr@.
 extractAllAttrValues :: String -> String -> [String]
 extractAllAttrValues text attr = nonNull $ map extract chunks where
   extract = flip stdRegex (attr ++ "=\"([^\"]*)\"")
